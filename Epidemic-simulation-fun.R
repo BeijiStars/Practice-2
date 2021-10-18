477e7f768d2369f7e2c47c936abb904cdb72ad4a
lispv <50668726+lispv@users.noreply.github.com>
2021-10-18 20:46:42 +0100

    Increase running speed

--- Epidemic-simulation-fun.R
+++ Epidemic-simulation-fun.R
@@ -17,7 +17,7 @@ epidemic <- function(n=5500000,ne=10,lambda=0.4/n,pei=1/3,pir=1/5,nd=100) {
   x[sample((1:length(x)), ne)] <- 1 ## start the epidemic by setting ne randomly chosen people to the E stat
   E[1] <- ne
   random_sample <- sample(1:n, size = 0.001*n) ## a random sample of 0.1% of the population to be recorded
-  
+  ind_lb <- which(beta < quantile(beta, 0.1))
   for (i in 2:nd) { ## loop over days
     u <- runif(n) ## uniform random deviates
     x[x==2&u<pir] <- 3 ## I -> R
@@ -26,16 +26,16 @@ epidemic <- function(n=5500000,ne=10,lambda=0.4/n,pei=1/3,pir=1/5,nd=100) {
     E[i] <- sum(x==1) ## exposed people of the whole pop on this day
     I[i] <- sum(x==2) ## infectious people of the whole pop on this day
     R[i] <- sum(x==3) ## recovered people of the whole pop on this day
-    E_lb[i] <- sum(x==1&beta<quantile(beta,0.1)) ## exposed people of the 10% of the pop with lowest beta
-    I_lb[i] <- sum(x==2&beta<quantile(beta,0.1)) ## infectious people of the 10% of the pop with lowest beta
-    R_lb[i] <- sum(x==3&beta<quantile(beta,0.1)) ## recoveries of the 10% of the pop with lowest beta
+    E_lb[i] <- sum(x[ind_lb]==1) ## exposed people of the 10% of the pop with lowest beta
+    I_lb[i] <- sum(x[ind_lb]==2) ## infectious people of the 10% of the pop with lowest beta
+    R_lb[i] <- sum(x[ind_lb]==3) ## recoveries of the 10% of the pop with lowest beta
     E_r[i] <- sum(x[random_sample]==1) ## exposed people of a random sample of 0.1% of the population
     I_r[i] <- sum(x[random_sample]==2) ## infectious people of a random sample of 0.1% of the population
     R_r[i] <- sum(x[random_sample]==3) ## recoveries of a random sample of 0.1% of the population
-    inew[i] <- (E[i]+I[i]) - (E[i-1]+I[i-1]) + (R[i]-R[i-1]) ## new infections of dayi in the whole pop
-    inew_lb[i] <- (E_lb[i]+I_lb[i]) - (E_lb[i-1]+I_lb[i-1]) + (R_lb[i]-R_lb[i-1]) ## new infections of dayi in the 10% of the pop with lowest beta
-    inew_r[i] <- (E_r[i]+I_r[i]) -(E_r[i-1]+I_r[i-1]) + (R_r[i]-R_r[i-1]) ## new infections of dayi of a random sample of 0.1% of the population
   }
+  inew[-1] <- E[-1] - E[-nd] + I[-1] - I[-nd] + (R[-1]-R[-nd]) ## new infections of each day in the whole pop
+  inew_lb[-1] <- E_lb[-1] - E_lb[-nd] + I_lb[-1] - I_lb[-nd] + (R_lb[-1]-R_lb[-nd]) ## new infections of each day in the 10% of the pop with lowest beta
+  inew_r[-1] <- E_r[-1] - E_r[-nd] + I_r[-1] -I_r[-nd]+ (R_r[-1]-R_r[-nd]) ## new infections of each day of a random sample of 0.1% of the populatio
   list(inew=inew,inew_lb=inew_lb,inew_r=inew_r)
 } ## Epidemic
 
@@ -56,4 +56,3 @@ lwd = 4, col = c(1,4,2), cex = 1.2, title = "New daily infections", bty = "n")
 text(which.max((inew_s)),max(inew_s), labels = paste("peak on day", which.max((inew_s))), col=1, adj=0)
 text(which.max((inew_lb_s)),max(inew_lb_s), labels = paste("peak on day", which.max((inew_lb_s))), col=4, adj=0)
 text(which.max((inew_r_s)),max(inew_r_s), labels = paste("peak on day", which.max((inew_r_s))), col=2, adj=0)
-
